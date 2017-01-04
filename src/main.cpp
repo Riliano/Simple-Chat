57a4c83f5a7378152423fbf15c96fe97c5fa1beb
Lazar <riliano@live.com>
2017-01-04 13:14:30 +0200

    Use cin to get messege

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include<SDL2/SDL.h>
 #include<SDL2/SDL_net.h>
 
-void input( char *messege, bool *done )
+void input( char *messege, int *size, bool *done )
 {
 	std::string temp;
 	std::getline( std::cin, temp );
@@ -13,13 +13,14 @@ void input( char *messege, bool *done )
 	{
 		*(messege+i) = temp[i];
 	}
+	*size = i;
 	*(messege+i) = '\0';
 	*done = true;
 }
 
 char messege[1024];
 
-int main()
+int main( int argc, char** argv )
 {
 	SDLNet_Init();
 
@@ -27,8 +28,13 @@ int main()
 	TCPsocket server, client, sock;
 	Uint16 port = 2314;
 	SDLNet_ResolveHost( &me, NULL, port );
-	SDLNet_ResolveHost( &they, "127.0.0.1", port );
+	SDLNet_ResolveHost( &they, "192.168.0.101", port );
 	server = SDLNet_TCP_Open( &me );
+	
+	bool doneWritingMessege = false;
+	char myMessege[1024];
+	int len;
+	std::thread t1( input, messege, &len, &doneWritingMessege );
 
 	std::cout<<SDLNet_GetError()<<std::endl;
 
@@ -44,7 +50,13 @@ int main()
 			std::cout<<"Did not recieve"<<std::endl;
 		if( sock )
 		{
-			SDLNet_TCP_Send( sock, "Hi", 3 );
+			if( doneWritingMessege )
+			{
+				t1.join();
+				doneWritingMessege = false;
+				t1 = std::thread( input, myMessege, &len, &doneWritingMessege ); 
+				SDLNet_TCP_Send( sock, myMessege, len );
+			}
 		}else
 			std::cout<<"Did not send"<<std::endl;
 
@@ -56,9 +68,7 @@ int main()
 
 	char messege[1024];
 
-	bool done = false;
-	std::thread t1( input, messege, &done );
-	while( true )
+		while( true )
 	{
 		if( done )
 		{
